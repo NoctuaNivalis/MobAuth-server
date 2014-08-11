d39325ddbcd8223375e80b00c0661b9d28b56cbc
Felix Van der Jeugt <fvanderjeugt@deloitte.com>
2014-08-11 14:09:21 +0200

    add edit/update/destroy to devices

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -2,14 +2,36 @@ class DevicesController < ApplicationController
 
   respond_to :js
 
+  before_action :find_user
+  before_action :find_device, only: [:edit, :update, :destroy]
+
   def index
-    @user = User.find params.require(:user_id)
     @devices = @user.devices
   end
 
   def create
-    @user = User.find params.require(:user_id)
     @device = @user.devices.create params.require(:device).permit(:name)
+  end
+
+  def edit
+  end
+
+  def update
+    @device.update params.require(:device).permit(:name)
+  end
+    
+  def destroy
+    @device.destroy
+  end
+
+  private
+
+  def find_user
+    @user = User.find params.require(:user_id)
+  end
+
+  def find_device
+    @device = Device.find params.require(:id)
   end
 
 end
